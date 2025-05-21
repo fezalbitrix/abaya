46841df8a16e8a7852aface62fc84901a1bbf171
fezalbitrix <fezal.multani@bitrixinfotech.com>
2025-05-21 14:46:55 +0530

    file-upload

--- assets/code/script.js
+++ assets/code/script.js
@@ -1,48 +1,48 @@
 const locoScroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    smoothMobile: false,
-    direction: 'vertical',
-    multiplier: 0.5,
-    inertia: 0.8,
-    repeat: false,
-  })
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+  smoothMobile: false,
+  direction: 'vertical',
+  multiplier: 0.5,
+  inertia: 0.8,
+  repeat: false,
+})
 
 
 function updateLoco() {
-    scroll.update();
-    console.log('loco scroll updated');
+  scroll.update();
+  console.log('loco scroll updated');
 }
 
 
 window.onresize = updateLoco;
 
-window.addEventListener('load', function() {
-    initScript();
+window.addEventListener('load', function () {
+  initScript();
 });
 
 new ResizeObserver(() => locoScroll.update()).observe(document.querySelector("[data-scroll-container]"))
 
 // Scroll update all pages
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   function ScrollUpdateDelay() {
-      setTimeout(function(){ locoScroll.update(); }, 500);
- 
+    setTimeout(function () { locoScroll.update(); }, 500);
+
   }
 
   ScrollUpdateDelay();
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(".gal").slice(0, 1).show();
-  $("#loadMore").on("click", function(e) {
-      e.preventDefault();
-      $(".gal:hidden").slice(0, 1).fadeIn(1000);
-      if ($(".gal:hidden").length == 0) {
-          $("#loadMore").text("").addClass("noContent");
-      }
+  $("#loadMore").on("click", function (e) {
+    e.preventDefault();
+    $(".gal:hidden").slice(0, 1).fadeIn(1000);
+    if ($(".gal:hidden").length == 0) {
+      $("#loadMore").text("").addClass("noContent");
+    }
   });
 
 })
